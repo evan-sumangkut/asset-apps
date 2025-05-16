6c842be446b83d52e2ad23c021515d79390ed06c
evan sumangkut <rizkyawliafajrin@gmail.com>
2025-05-16 14:57:41 +0700

    fix layout

--- apps/resources/views/layouts/app.blade.php
+++ apps/resources/views/layouts/app.blade.php
@@ -13,8 +13,6 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @livewireStyles
-
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
@@ -22,7 +20,7 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            @include('../navigation-menu')
 
             <!-- Page Heading -->
             @if (isset($header))
